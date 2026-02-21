b0eb42b63e8c195e87e2ddc0bd94f58afc7fa654
Timotej Avsec <avsectimotej1@gmail.com>
2026-02-21 18:09:22 +0100

    Add dashboard widgets

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Widgets\ClientsOverview;
 use App\Http\Middleware\IsAdminMiddleware;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
@@ -39,7 +40,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
-                AccountWidget::class,
+                ClientsOverview::class
             ])
             ->middleware([
                 EncryptCookies::class,
