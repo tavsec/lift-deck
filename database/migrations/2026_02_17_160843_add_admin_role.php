6f66a84a58e1319ad3da179971676e5050661e3a
Timotej Avsec <avsectimotej1@gmail.com>
2026-02-28 14:38:54 +0100

    FIx admin migration

--- database/migrations/2026_02_17_160843_add_admin_role.php
+++ database/migrations/2026_02_17_160843_add_admin_role.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -11,10 +10,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-
-        Schema::table('users', function (Blueprint $table) {
-            $table->enum('role', ['coach', 'client', 'admin'])->change();
-        });
+        DB::statement('ALTER TABLE users DROP CONSTRAINT IF EXISTS users_role_check');
+        DB::statement("ALTER TABLE users ADD CONSTRAINT users_role_check CHECK (role IN ('coach', 'client', 'admin'))");
     }
 
     /**
@@ -22,8 +19,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->enum('role', ['coach', 'client'])->change();
-        });
+        DB::statement('ALTER TABLE users DROP CONSTRAINT IF EXISTS users_role_check');
+        DB::statement("ALTER TABLE users ADD CONSTRAINT users_role_check CHECK (role IN ('coach', 'client'))");
     }
 };
