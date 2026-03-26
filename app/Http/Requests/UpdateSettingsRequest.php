28a455d85e55b5707a30e713e1f4577d3a861743
Timotej Avsec <avsectimotej1@gmail.com>
2026-03-26 12:58:20 +0100

    fix: add authorize method and PHPDoc to UpdateSettingsRequest

--- app/Http/Requests/UpdateSettingsRequest.php
+++ app/Http/Requests/UpdateSettingsRequest.php
@@ -8,6 +8,16 @@ use Illuminate\Validation\Rule;
 
 class UpdateSettingsRequest extends FormRequest
 {
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
     public function rules(): array
     {
         return [
