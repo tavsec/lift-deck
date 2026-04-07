218bbbf8f60ea5f50249c70737a95bc8e8bc8d4f
Timotej Avsec <avsectimotej1@gmail.com>
2026-04-07 05:18:24 +0200

    Remove csrf from webhook

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,9 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\SetLocale::class,
         ]);
         $middleware->trustProxies('*');
+        $middleware->validateCsrfTokens(except: [
+            'cashier/webhook',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
